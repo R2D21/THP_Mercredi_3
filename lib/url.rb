79dde01fe95836be6a028c20b94a981d293f8e53
remi <remi.boivin@epitech.eu>
2018-07-18 18:10:59 +0200

    commit

--- lib/url.rb
+++ lib/url.rb
@@ -2,28 +2,28 @@ class Url
   def   initialize
 
   end
+
   def     get_uri(page_url)
-  links = []
-  i = 0;
-  page = Nokogiri::HTML(open(page_url))
-  news_links = page.css("a").select{|link| link['href']}
-  news_links.each do |link|
-    links[i] = link['href']
-    i +=1;
+    links = []
+    i = 0;
+    page = Nokogiri::HTML(open(page_url))
+    news_links = page.css("a").select{|link| link['href']}
+    news_links.each do |link|
+      links[i] = link['href']
+      i +=1;
+    end
+    return links
   end
-  return links
-end
 
-def     get_uri_code(page_url)
-  links = []
-  i = 0;
-  page = Nokogiri::HTML(open(page_url))
-  news_links = page.css("a").select{|link| link['title']}
-  news_links.each do |link|
-    links[i] = link['title']
-    i +=1;
+  def     get_uri_code(page_url)
+    links = []
+    i = 0;
+    page = Nokogiri::HTML(open(page_url))
+    news_links = page.css("a").select{|link| link['title']}
+    news_links.each do |link|
+      links[i] = link['title']
+      i +=1;
+    end
+    return links
   end
-  return links
-end
-
 end
