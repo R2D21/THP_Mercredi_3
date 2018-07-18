c555e065ce1fe61948b381dcc7dd3e641ef9445c
remi <remi.boivin@epitech.eu>
2018-07-18 14:23:54 +0200

    commit

--- my_app/lib/scrapper.rb
+++ my_app/lib/scrapper.rb
@@ -49,80 +49,80 @@ class   Scrapper
   return my_hash
   end
 
-def     get_address(mairie)
-  links = []
-  i = 0;
-  page = Nokogiri::HTML(open(page_url))
-  news_links = page.css("a").select{|link| link['href']}
-  news_links.each do |link|
-    links[i] = link['href']
-    i +=1;
-  end
-  return links
-end
-
-def     check_country(links)
-  i = 0;
-  links.each do |word|
-    if word.include?("val-d-oise")
-      return (i);
+  def     get_address(mairie)
+    links = []
+    i = 0;
+    page = Nokogiri::HTML(open(page_url))
+    news_links = page.css("a").select{|link| link['href']}
+    news_links.each do |link|
+      links[i] = link['href']
+      i +=1;
     end
-    i +=1
+    return links
   end
-end
 
-def     aff(array)
-
-  array.each do |word|
-    puts word
+  def     check_country(links)
+    i = 0;
+    links.each do |word|
+      if word.include?("val-d-oise")
+        return (i);
+      end
+      i +=1
+    end
   end
-end
 
-def get_all_mails(links)
-  j = 0
-  emails = []
-  links.each do |link|
-    emails[j] = get_email(link)
-    puts emails[j]
-    j +=1
+  def     aff(array)
+
+    array.each do |word|
+      puts word
+    end
   end
-  return emails
-end
 
-def get_email(url)
-
-  page = Nokogiri::HTML(open(url))
-  page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
-end
-
-def     perform()
-  i = 0;
-  j = 0;
-  news_links = get_uri(@PAGE_URL)
-  link = @PAGE_URL + '/'
-  tmp = []
-  temp = ""
-  link += news_links[check_country(news_links)]
-  get_uri(link).each do |lin|
-    tmp[i] = lin.split('.')[1]
-    if tmp[i][0] == '/'
-      temp = @PAGE_URL + tmp[i]
-      links[j] = temp
+  def get_all_mails(links)
+    j = 0
+    emails = []
+    links.each do |link|
+      emails[j] = get_email(link)
+      puts emails[j]
       j +=1
     end
-    i +=1
-    end
-  list = create_hash(get_all_mails(links),links)
-  i = 6
-  tmp = parse_string(names)
-  i = 0
-  j = 0
-  while i < tmp.size - 15
-    names[j] = tmp[i]
-    i +=1
-    j +=1
+    return emails
   end
-  my_hash = create_hash(links, get_all_mails(links))
-  puts "#{my_hash}"
-end
+
+  def get_email(url)
+
+    page = Nokogiri::HTML(open(url))
+    page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
+  end
+
+  def     perform()
+    i = 0;
+    j = 0;
+    news_links = get_uri(@PAGE_URL)
+    link = @PAGE_URL + '/'
+    tmp = []
+    temp = ""
+    link += news_links[check_country(news_links)]
+    get_uri(link).each do |lin|
+      tmp[i] = lin.split('.')[1]
+      if tmp[i][0] == '/'
+        temp = @PAGE_URL + tmp[i]
+        links[j] = temp
+        j +=1
+      end
+      i +=1
+    end
+    list = create_hash(get_all_mails(links),links)
+    i = 6
+    tmp = parse_string(names)
+    i = 0
+    j = 0
+    while i < tmp.size - 15
+      names[j] = tmp[i]
+      i +=1
+      j +=1
+    end
+    my_hash = create_hash(links, get_all_mails(links))
+    puts "#{my_hash}"
+  end
 end
