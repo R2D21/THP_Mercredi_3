eccc42b807ed5f68bb3e0c043b89288c117d758d
remi <remi.boivin@epitech.eu>
2018-07-18 18:51:07 +0200

    commit

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require "bundler"
 Bundler.require
 $:.unshift File.expand_path("./../lib", __FILE__)
 require 'google_drive'
-session = GoogleDrive::Session.from_config("config.json")
-ws = session.spreadsheet_by_key("1oe3-ps6UiH55_Wvb0WEG5XJ6-zDbtz2YB-egLiZITEM").worksheets[0]
+# session = GoogleDrive::Session.from_config("config.json")
+# ws = session.spreadsheet_by_key("1oe3-ps6UiH55_Wvb0WEG5XJ6-zDbtz2YB-egLiZITEM").worksheets[0]
 # p ws[22,1] = "Salut tout le monde"
 # ws.save
 scrapper = Scrapper.new("http://annuaire-des-mairies.com/val-d-oise.html")
