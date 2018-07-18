852d18bfb6b6ea080fb4d89ccbcbf0f800bbd78d
remi <remi.boivin@epitech.eu>
2018-07-18 17:16:04 +0200

    commit

--- app.rb
+++ app.rb
@@ -3,3 +3,4 @@ Bundler.require
 $:.unshift File.expand_path("./../lib", __FILE__)
 require 'google_drive'
 session = GoogleDrive::Session.from_config("config.json")
+ws = session.spreadsheet_by_key("1smypkyAz4STrKO4Zkos5Z4UPUJKvvgIza32LnlQ7OGw").worksheets[0]
