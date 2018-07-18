c700d81639a3a557295234518afeb3e024f464f0
remi <remi.boivin@epitech.eu>
2018-07-18 18:49:35 +0200

    commit

--- app.rb
+++ app.rb
@@ -6,3 +6,4 @@ session = GoogleDrive::Session.from_config("config.json")
 ws = session.spreadsheet_by_key("1oe3-ps6UiH55_Wvb0WEG5XJ6-zDbtz2YB-egLiZITEM").worksheets[0]
 # p ws[22,1] = "Salut tout le monde"
 # ws.save
+scrapper = Scrapper.new("http://annuaire-des-mairies.com/val-d-oise.html")
