2f574055c66b7f3588dfd72095ceeb0776161303
remi <remi.boivin@epitech.eu>
2018-07-18 16:40:11 +0200

    commit

--- my_app/app.rb
+++ my_app/app.rb
@@ -1,6 +1,5 @@
 require "bundler"
 Bundler.require
 $:.unshift File.expand_path("./../lib", __FILE__)
-require 'scrapper'
 require 'google-drive'
 session = GoogleDrive::Session.from_config("config.json")
