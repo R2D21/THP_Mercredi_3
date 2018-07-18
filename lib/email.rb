d14c20fae15dba22338df0f5bdaa3289636812d7
remi <remi.boivin@epitech.eu>
2018-07-18 17:41:50 +0200

    commit

--- lib/email.rb
+++ lib/email.rb
@@ -2,4 +2,20 @@ class Email
   def   initialize
 
   end
+
+  def get_all_mails(links)
+    j = 0
+    emails = []
+    links.each do |link|
+      emails[j] = get_email(link)
+      puts emails[j]
+      j +=1
+    end
+    return emails
+  end
+
+ def get_email(url)
+        page = Nokogiri::HTML(open(url))
+        page.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').text
+ end
 end
