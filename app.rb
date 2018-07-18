7d87c039699f36ae0d75bbbf067bba13df020093
remi <remi.boivin@epitech.eu>
2018-07-18 18:52:29 +0200

    commit

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ Bundler.require
 $:.unshift File.expand_path("./../lib", __FILE__)
 require 'google_drive'
 load "scrapper"
-
+load "url"
 # session = GoogleDrive::Session.from_config("config.json")
 # ws = session.spreadsheet_by_key("1oe3-ps6UiH55_Wvb0WEG5XJ6-zDbtz2YB-egLiZITEM").worksheets[0]
 # p ws[22,1] = "Salut tout le monde"
