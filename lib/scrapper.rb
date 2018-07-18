cfbefddc3366862e2cd5b974345f159bbc352fd9
remi <remi.boivin@epitech.eu>
2018-07-18 18:55:07 +0200

    commit

--- lib/scrapper.rb
+++ lib/scrapper.rb
@@ -1,7 +1,7 @@
 class Scrapper
   def   initialize(link)
     url = Url.new(link)
-    links = url.get_uri(link)
+    links = url.get_uri()
     emails = Email.new(links)
     mails = emails.get_all_mails
   end
