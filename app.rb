cb1ef161adb867db0581948633ed9959146f1c57
remi <remi.boivin@epitech.eu>
2018-07-18 17:19:02 +0200

    commit

--- app.rb
+++ app.rb
@@ -4,4 +4,4 @@ $:.unshift File.expand_path("./../lib", __FILE__)
 require 'google_drive'
 session = GoogleDrive::Session.from_config("config.json")
 ws = session.spreadsheet_by_key("1smypkyAz4STrKO4Zkos5Z4UPUJKvvgIza32LnlQ7OGw").worksheets[0]
-p
+p ws[2,1]
