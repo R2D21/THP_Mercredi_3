0c8d4d4513dcc3b9f8d6ccc4864ba73eb1de9523
remi <remi.boivin@epitech.eu>
2018-07-18 12:46:35 +0200

    commit

---
+++ depute.rb
@@ -0,0 +1,58 @@
+# coding: utf-8
+require "nokogiri"
+require "open-uri"
+
+@page_url = "http://www2.assemblee-nationale.fr/deputes/liste/alphabetique"
+class   Deputes
+  def     deputes_names()
+    links = []
+    i = 0;
+    j = 0
+    page = Nokogiri::HTML(open(@page_url))
+    news_links = page.css("ul[class=col3]").css("li").css("a")
+    news_links.each do |link|
+      links[i] = link.text
+      i +=1;
+    end
+    return links
+  end
+
+  def     deputes_url()
+    links = []
+    i = 0;
+    j = 0
+    page = Nokogiri::HTML(open(@page_url))
+    news_links = page.css("ul[class=col3]").css("li").css("a").select{|link| link['href']}
+    news_links.each do |link|
+      links[i] =  @url + link['href']
+      i +=1;
+    end
+    return   links
+  end
+
+  @url = "http://www2.assemblee-nationale.fr"
+
+  def     get_email(url)
+    email = []
+    page = Nokogiri::HTML(open(url))
+    page.css('a[@href ^="mailto:"]').each do |element|
+    email << element["href"]
+    email[0][0..6] = ''
+    break
+    end
+    return email
+  end
+
+  def     create_array_hash(first_name,last_name,emails)
+    array = []
+    my_hash = {}
+    i = 0;
+    while i < first_name.size
+      my_hash = {:first_name => first_name[i],:last_name => last_name[i],:email => emails[i]}
+      array.push(my_hash);
+      i +=1
+    end
+    return array
+  end
+end
+perform
