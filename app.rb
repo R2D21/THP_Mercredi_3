7a4c9bb7860ded96e181f8c6d5c6434ad4cb1261
remi <remi.boivin@epitech.eu>
2018-07-18 17:31:58 +0200

    commit

--- app.rb
+++ app.rb
@@ -5,3 +5,4 @@ require 'google_drive'
 session = GoogleDrive::Session.from_config("config.json")
 ws = session.spreadsheet_by_key("1oe3-ps6UiH55_Wvb0WEG5XJ6-zDbtz2YB-egLiZITEM").worksheets[0]
 p ws[22,1] = "Salut tout le monde"
+ws.save
