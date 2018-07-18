f7a7e7b900d83b03816b6ca862c6fe8f64afdcbc
remi <remi.boivin@epitech.eu>
2018-07-18 18:26:23 +0200

    commit

--- lib/url.rb
+++ lib/url.rb
@@ -14,16 +14,4 @@ class Url
     end
     return links
   end
-
-  def     get_uri_code()
-    links = []
-    i = 0;
-    page = Nokogiri::HTML(open(@page_url))
-    news_links = page.css("a").select{|link| link['title']}
-    news_links.each do |link|
-      links[i] = link['title']
-      i +=1;
-    end
-    return links
-  end
 end
